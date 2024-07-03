566cb49dba768fda5ecbb2caa2af20726ca26b66
Hojung Kim <tora4094@inha.edu>
2024-07-03 23:54:36 +0900

    feature: 플러그인이 활성화 될 때 YML 파일의 유저 데이터를 전부 가져오도록 구현함
    - 소규모 플레이이기 때문에 접속하면 메모리에 올리는 작업을 하지 않아도 된다고 판단함.

--- src/main/kotlin/kr/eme/plugin/SemiMoney.kt
+++ src/main/kotlin/kr/eme/plugin/SemiMoney.kt
@@ -3,6 +3,7 @@ package kr.eme.plugin;
 import kr.eme.plugin.commands.MoneyCommand
 import kr.eme.plugin.listener.MoneyListener
 import kr.eme.plugin.managers.FileManager
+import kr.eme.plugin.managers.UserManager
 import kr.eme.plugin.objects.User
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
@@ -11,16 +12,16 @@ class SemiMoney: JavaPlugin() {
     override fun onEnable() {
         //FileManager 에 DataFolder 전달
         FileManager.init(dataFolder)
-
+        //모든 User 의 정보를 불러옴
+        FileManager.loadAllUsers()
         //이벤트 리스너 등록
         server.pluginManager.registerEvents(MoneyListener, this)
-
         //커맨드 등록
         getCommand("MoneyCommand")?.setExecutor(MoneyCommand)
-        test()
         logger.info("Server enable!")
     }
     override fun onDisable() {
+        UserManager.saveAllUsers()
         logger.info("Server disable!")
     }
 
