c6bb119231368044c991e637f5e90ea9cf7b881c
Hojung Kim <tora4094@inha.edu>
2024-07-03 23:56:10 +0900

    feature: onJoin 구현

--- src/main/kotlin/kr/eme/plugin/listener/MoneyListener.kt
+++ src/main/kotlin/kr/eme/plugin/listener/MoneyListener.kt
@@ -1,19 +1,26 @@
 package kr.eme.plugin.listener
 
+import kr.eme.plugin.managers.FileManager
+import kr.eme.plugin.managers.UserManager
+import kr.eme.plugin.objects.User
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.player.PlayerQuitEvent
 
 object MoneyListener : Listener {
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
         val player = e.player
         val uuid = player.uniqueId
-    }
-    @EventHandler
-    fun onQuit(e: PlayerQuitEvent) {
-        val player = e.player
-        val uuid = player.uniqueId
+        //첫 접속인지 확인
+        var user = UserManager.getUser(uuid)
+        //첫 접속일 경우 새로운 User 객체를 생성함.
+        if (user == null) {
+            user = User(uuid, player.name, 0)
+            UserManager.addUser(user)
+            FileManager.saveUserData(user) // 첫 접속 시 user_data.yml 파일에 추가
+        } else {
+            user.name = player.name //만약 닉네임이 변경되었을 시에는 업데이트를 해줌
+        }
     }
 }
