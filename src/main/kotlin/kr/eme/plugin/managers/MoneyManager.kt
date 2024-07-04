77dc473b2b9bc5f0576155e377629de9abcf5e93
Hojung Kim <tora4094@inha.edu>
2024-07-04 23:09:29 +0900

    update: MoneyManager subtractMoney 함수 수량 예외 문구 추가

--- src/main/kotlin/kr/eme/plugin/managers/MoneyManager.kt
+++ src/main/kotlin/kr/eme/plugin/managers/MoneyManager.kt
@@ -1,5 +1,6 @@
 package kr.eme.plugin.managers
 
+import org.bukkit.Bukkit
 import java.util.*
 
 object MoneyManager {
@@ -30,6 +31,7 @@ object MoneyManager {
     /**
      * Add money
      *
+     * 수표를 통하여만 작동하므로 무조건 이 구문이 실행될 시 플레이어는 온라인임
      * @param uuid player's UUID
      * @param amount
      * @return true or false
@@ -43,13 +45,20 @@ object MoneyManager {
     /**
      * Subtract money
      *
+     * 수표를 통하여만 작동하므로 무조건 이 구문이 실행될 시 플레이어는 온라인임
      * @param uuid player's UUID
      * @param amount
      * @return true or false
      */
     fun subtractMoney(uuid: UUID, amount: Int) : Boolean {
         val user = UserManager.getUser(uuid) ?: return false
-        if (user.money < amount) return false
+        //UUID 를 통하여 플레이어를 검색함
+        val player = Bukkit.getPlayer(uuid) ?: return false
+        if (amount <= 0) {
+            player.sendMessage("금액은 0보다 커야 합니다!")
+            return false
+        }
+        if (user.money <= amount) return false
         user.money -= amount
         return true
     }
